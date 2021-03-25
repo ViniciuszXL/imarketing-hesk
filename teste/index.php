a972825a185fb84872437ad02512ce491f0e2281
Marcus Vinicius <vinicius@imarketing.local>
2021-03-25 11:07:09 -0400

    Debug

--- teste/index.php
+++ teste/index.php
@@ -12,10 +12,16 @@
     echo 'Filename is ' . $filename . ' - ';
     echo 'Destination is ' . $destination . ' - ';
 
-    if (move_uploaded_file($filename, $destination)) {
-        echo 'Successfully moved oie.txt to other_folder!';
+    if (rename($filename, $destination)) {
+        echo 'Successfully moved oie.txt to other_folder! [1] - ';
     } else {
-        echo 'An error ocurred in move the oie.txt to other_folder!';
+        echo 'An error ocurred in move the oie.txt to other_folder! [1] - ';
+    }
+
+    if (move_uploaded_file($filename, $destination)) {
+        echo 'Successfully moved oie.txt to other_folder! [2] - ';
+    } else {
+        echo 'An error ocurred in move the oie.txt to other_folder! [2] - ';
     }
 
 ?>
